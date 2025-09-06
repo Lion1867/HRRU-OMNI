63b22e9c0ec22d8a7e6a13c0f2b767cd18894064
Lion1867 <127045092+Lion1867@users.noreply.github.com>
2025-09-06 19:38:26 +0500

    Добавлен .bat файл для запуска проекта

---
+++ startup.bat
@@ -0,0 +1,14 @@
+@echo off
+echo Запуск всех сервисов...
+
+:: Django
+start "Django Server" cmd /k "cd /d E:\diplom_final_project\IT-WorkRu\myplatform && E:\diplom_final_project\IT-WorkRu\platform_venv\Scripts\python.exe manage.py runserver"
+
+:: Text/Audio API
+start "Text Audio API" cmd /k "cd /d E:\diplom_final_project\interview_module && E:\diplom_final_project\interview_module\.venv\Scripts\python.exe text_audio_questions_api.py"
+
+:: Talking Avatar API
+start "Real Time HR" cmd /k "cd /d E:\diplom_final_project\Real_time_HR && E:\diplom_final_project\Real_time_HR\.venv\Scripts\python.exe real_talking_avatar_api.py"
+
+echo Все сервисы запущены.
+exit
